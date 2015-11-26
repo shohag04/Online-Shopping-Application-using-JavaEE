31a6a8db2a0eea9b72e98cf29ebd35bda07b3d25
shohag04 <shohag.memphis@gmail.com>
2015-11-25 23:46:59 -0600

    Create Item.java
    This is simple POJO class that holds attributes of Item which are printed to the user

---
+++ Item.java
@@ -0,0 +1,38 @@
+package com.shohag.shopping.dao;
+
+public class Item {
+	private Integer id = 0;
+	private String name = null;
+	private Integer price = 0;
+	
+	public Item(Integer id, String name, Integer price) {
+		this.id = id;
+		this.name = name;
+		this.price = price;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getPrice() {
+		return price;
+	}
+
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+	
+}
